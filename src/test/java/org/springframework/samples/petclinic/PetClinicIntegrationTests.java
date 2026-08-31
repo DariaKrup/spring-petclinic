b2a522b2acfc0b3e6cae69a80f913b2a852c8f27
Daria Krupkina <55836255+DariaKrup@users.noreply.github.com>
2026-08-31 13:29:34 +0200

    Update PetClinicIntegrationTests.java

--- src/test/java/org/springframework/samples/petclinic/PetClinicIntegrationTests.java
+++ src/test/java/org/springframework/samples/petclinic/PetClinicIntegrationTests.java
@@ -56,6 +56,13 @@ public class PetClinicIntegrationTests {
 		assertThat(result.getStatusCode()).isEqualTo(HttpStatus.OK);
 	}
 
+	@Test
+	void testSecondOwnerDetails() {
+		RestTemplate template = builder.rootUri("http://localhost:" + port).build();
+		ResponseEntity<String> result = template.exchange(RequestEntity.get("/owners/1").build(), String.class);
+		assertThat(result.getStatusCode()).isEqualTo(HttpStatus.OK);
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(PetClinicApplication.class, args);
 	}
